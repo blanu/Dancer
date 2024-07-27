88a30c5e8d3dc3a7a6cbb7c5374d4f1f9b495095
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2024-07-27 13:06:49 -0500

    Initial Commit

---
+++ Morse/MorseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorseApp.swift
+//  Morse
+//
+//  Created by Dr. Brandon Wiley on 7/27/24.
+//
+
+import SwiftUI
+
+@main
+struct MorseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
